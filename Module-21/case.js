cda145d79cc6866c10d9321547f966183ee52a60
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-08-04 21:46:45 +0600

    case added

---
+++ Module-21/case.js
@@ -0,0 +1,10 @@
+const userName = 'blackPink';
+const userInput = 'blackpinK';
+console.log();
+
+if(userName.toLowerCase() === userInput.toLowerCase()){
+    console.log('valid user')
+}
+else{
+    console.log('invalid user');
+}
