d7039ca564df1963bd61913f2479f5a910615275
Sifatul Islam <mdshepatul.islam@gmail.com>
2023-08-05 23:31:49 +0600

    module 22 added

---
+++ Module-22/array.js
@@ -0,0 +1,33 @@
+const country = 'Bangladesh';
+const age = 19;
+const isIndependent = true;
+const students = {id: 121, class: 11, name: 'Sifat'};
+const friends = [13, 45, 532, 565, 24, 850];
+function add(num1, num2){
+    return num1 + num2
+}
+
+
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isIndependent);
+console.log(typeof students);
+// check array using Array.isArray
+console.log(Array.isArray(friends));
+console.log(typeof add);
+
+// .........................
+
+console.log(friends.includes(19));
+console.log(friends.includes(21));
+
+
+if(friends.indexOf(252) !== -1){
+
+}
+
+// concat
+
+const newFriendAge = [12, 13, 14, 48, 55, 44]
+const allFriends = newFriendAge.concat(friends);
+console.log(allFriends);
